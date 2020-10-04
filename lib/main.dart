d9661c5a505b0e119175edf8bd72a22b40bfc077
sumitvarun <sumitvarun0781@gmail.com>
2020-10-04 15:44:09 +0530

    add OutlineButton to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:mycard/profile.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,81 +10,126 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Center(
-                child: CircleAvatar(
-                  radius: 55.0,
-                  backgroundColor: Colors.lightGreenAccent,
-                  backgroundImage: AssetImage('images/sumitkkkk.jpg'),
-                ),
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.teal,
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Center(
+              child: CircleAvatar(
+                radius: 55.0,
+                backgroundColor: Colors.lightGreenAccent,
+                backgroundImage: AssetImage('images/sumitkkkk.jpg'),
               ),
-              Text(
-                'Sumit Varun',
-                style: TextStyle(
-                  fontFamily: 'Pacifico',
-                  color: Colors.white,
-                  fontSize: 30.0,
-                  //fontWeight: FontWeight.bold,
-                ),
+            ),
+            Text(
+              'Sumit Varun',
+              style: TextStyle(
+                fontFamily: 'Pacifico',
+                color: Colors.white,
+                fontSize: 30.0,
+                //fontWeight: FontWeight.bold,
               ),
-              Text(
-                'FLUTTER  DEVELOPER',
-                style: TextStyle(
-                  fontFamily: 'Exo2',
-                  color: Colors.tealAccent,
-                  fontSize: 20.0,
-                ),
+            ),
+            Text(
+              'FLUTTER  DEVELOPER',
+              style: TextStyle(
+                fontFamily: 'Exo2',
+                color: Colors.tealAccent,
+                fontSize: 20.0,
               ),
-              SizedBox(
-                height: 20.0,
-                width: 155.0,
-                child: Divider(
-                  color: Colors.tealAccent,
-                ),
+            ),
+            SizedBox(
+              height: 20.0,
+              width: 155.0,
+              child: Divider(
+                color: Colors.tealAccent,
               ),
-              Card(
-                  color: Colors.white,
-                  margin:
-                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                  child: ListTile(
-                    leading: Icon(
-                      Icons.phone_android,
-                      color: Colors.teal,
-                    ),
-                    title: Text(
-                      '+91-123 456 789',
-                      style: TextStyle(
-                        color: Colors.teal,
-                        fontFamily: 'Exo2',
-                        fontSize: 20.0,
-                      ),
-                    ),
-                  )),
-              Card(
+            ),
+            Card(
                 color: Colors.white,
                 margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
                 child: ListTile(
                   leading: Icon(
-                    Icons.email,
+                    Icons.phone_android,
                     color: Colors.teal,
                   ),
                   title: Text(
-                    'sumit.varun@rocketmail.com',
+                    '+91-123 456 789',
                     style: TextStyle(
                       color: Colors.teal,
                       fontFamily: 'Exo2',
-                      fontSize: 16.0,
+                      fontSize: 20.0,
                     ),
+                  ),
+                )),
+            Card(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: ListTile(
+                leading: Icon(
+                  Icons.email,
+                  color: Colors.teal,
+                ),
+                title: Text(
+                  'sumit.varun@rocketmail.com',
+                  style: TextStyle(
+                    color: Colors.teal,
+                    fontFamily: 'Exo2',
+                    fontSize: 16.0,
                   ),
                 ),
               ),
-            ],
-          ),
+            ),
+            SizedBox(
+              height: 50.0,
+            ),
+            SizedBox(
+                width: 200.0,
+                height: 100.0,
+                child: OutlineButton(
+                  borderSide: BorderSide(width: 2.0, color: Colors.cyanAccent),
+                  padding: EdgeInsets.only(left: 22.0),
+                  color: Colors.teal[800],
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(35.0),
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => Profile()),
+                    );
+                  },
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Image.asset(
+                        'images/home.png',
+                        height: 40,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(20.0),
+                        child: Text(
+                          'PROFILE',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20.0,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                )),
+          ],
         ),
       ),
     );
